311cd461b89a4a87bc09d4728960d944d25d4b39
Andrey Baldin <andreybal@dmdevelopment.ru>
2021-05-09 23:58:38 +0700

    Add circle

---
+++ AreaCalculatorPRO/AreaCalculatorPRO/Shapes/Circle.cs
@@ -0,0 +1,31 @@
+﻿using AreaCalculatorPRO.Constants.Errors.Messages;
+using AreaCalculatorPRO.Shapes.Base;
+using Dawn;
+using System;
+
+namespace AreaCalculatorPRO.Shapes
+{
+    /// <summary>
+    /// Circle.
+    /// </summary>
+    public class Circle : Shape
+    {
+        /// <summary>
+        /// Radius.
+        /// </summary>
+        public double Radius { get; }
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        /// <param name="radius">Circle radius.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Circle radius value is less than or equal to 0.</exception>
+        public Circle (double radius)
+        {
+            Guard.Argument(radius, nameof(radius)).NotNegative(message: radius => ExceptionMessages.CircleRadiusCannotBeNegative)
+                .GreaterThan(0, message: (radius, x) => ExceptionMessages.CircleRadiusMustBeGreaterThanZero);
+
+            Radius = radius;
+        }
+    }
+}
