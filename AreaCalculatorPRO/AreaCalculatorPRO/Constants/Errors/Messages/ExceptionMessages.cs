32304dbac571cf03846706253910a8d16ae76d1d
Detolls <45891355+Detolls@users.noreply.github.com>
2021-05-10 00:01:45 +0700

    Develop (#1)
    * Add base shape
    
    * Add exception messages
    
    * Add triangle
    
    * Add circle
    
    * Add area calculator
    
    * Add shape tests (xUnit)
    
    Co-authored-by: Andrey Baldin <andreybal@dmdevelopment.ru>

---
+++ AreaCalculatorPRO/AreaCalculatorPRO/Constants/Errors/Messages/ExceptionMessages.cs
@@ -0,0 +1,30 @@
+﻿namespace AreaCalculatorPRO.Constants.Errors.Messages
+{
+    /// <summary>
+    /// Static class for storing exception messages.
+    /// </summary>
+    public static class ExceptionMessages
+    {
+        #region Shape messages
+
+        public static string ShapeMustNotBeNull => "Shape must not be null.";
+        public static string ShapeIsNotRecognized => "Shape is not a recognized shape.";
+
+        #endregion
+
+        #region Triangle messages
+
+        public static string TriangleSideCannotBeNegative => "The side of a triangle cannot be negative.";
+        public static string TriangleSideMustBeGreaterThanZero => "The side of the triangle must be greater than 0.";
+        public static string TriangleWithGivenSidesDoesNotExists => "Triangle with given sides does not exist.";
+
+        #endregion
+
+        #region Circle messages
+
+        public static string CircleRadiusCannotBeNegative => "The radius of a circle cannot be negative.";
+        public static string CircleRadiusMustBeGreaterThanZero => "The radius of a circle must be greater than 0.";
+
+        #endregion
+    }
+}
